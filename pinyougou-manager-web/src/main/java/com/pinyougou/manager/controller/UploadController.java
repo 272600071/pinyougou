c8a2f0de2af81cf43c4653fa071b423fd2952361
272600071 <272600071@qq.com>
2018-05-06 00:38:47 +0800

    add some good 功能(add portal-web模块)

---
+++ pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/UploadController.java
@@ -0,0 +1,36 @@
+package com.pinyougou.manager.controller;
+
+import com.pinyougou.util.FastDFSClient;
+import entity.Result;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author jack
+ * @version 1.0
+ * @description com.pinyougou.shop.controller
+ * @date 2018/4/28
+ */
+@RestController
+public class UploadController {
+    @Value("${IMAGE_SERVER_URL}")
+    private String IMAGE_SERVER_URL;
+
+    @RequestMapping("/upload")
+    public Result upload(MultipartFile file){
+        String filename = file.getOriginalFilename();
+        String extName = filename.substring(filename.lastIndexOf("."));
+        try {
+            FastDFSClient fastDFSClient=new FastDFSClient("classpath:config/fdfs_client.properties");
+            String path = fastDFSClient.uploadFile(file.getBytes(), extName);
+            String url = IMAGE_SERVER_URL + path;
+            return new Result(true,url);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false,"上传失败");
+        }
+
+    }
+}
